2b20e71660f72d209e539b630551e3abc0b5cc62
Ville Sihvola <ville.sihvola8@gmail.com>
2021-03-15 22:16:17 +0200

    startup

---
+++ src/main/java/com/example/aquafi/AquafiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aquafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquafiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquafiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aquafi/AquafiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquafi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquafiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
